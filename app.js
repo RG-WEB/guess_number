7307d1d40809c1b86b1b45b7d2f23ba7462e9506
Gino Rocca <roccagino5@gmail.com>
2023-05-12 12:06:40 +0200

    finalisation du projet

--- app.js
+++ app.js
@@ -22,15 +22,36 @@
 
 function guessNumber() {
   const randomNumber = Math.floor(Math.random() * 100) + 1;
-  
-  const input = prompt("Saisissez un nombre entre 1 et 100");
-  const myGuessNumber = randomNumber(input);
+  const LifeMAX = 6;
+  let lifePoint = LifeMAX;
 
-    console.log("Tu as gagné !");
-  } else if { (myGuessNumber > randomNumber) 
-    console.log(`${myGuessNumber}C'est plus Réessaie!`);
-  } else if { (myGuessNumber < randomNumber) }
-    console.log(`${myGuessNumber}C'est moins Réessaie!`);
+  while (lifePoint > 0) {
+    const input = prompt("Saisissez un nombre entre 1 et 100");
+
+    myGuessNumber = Number(input);
+
+    if (myGuessNumber < randomNumber) {
+      console.log(`${input} : C'est plus`);
+      lifePoint--;
+    } else if (myGuessNumber > randomNumber) {
+      console.log(`${input} : C'est moins`);
+      lifePoint--;
+    } else {
+      console.log("Félicitations, vous avez trouvé !");
+      return;
+    }
+
+    if (lifePoint > 0) {
+      console.log(
+        `Essai${lifePoint > 1 ? "s" : ""} restant${
+          lifePoint > 1 ? "s" : ""
+        } : ${lifePoint}`
+      );
+    }
+  }
+
+  console.log(`Perdu :( Le résultat était : ${randomNumber}`);
+}
 
 guessNumber();
 
